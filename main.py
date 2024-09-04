b3d80f99e08d4940d88b1176e1bbcec7b4f3ea24
Rosario Laface <lafacerosario@gmail.com>
2024-09-04 15:42:24 +0200

    init

--- main.py
+++ main.py
@@ -1,52 +1,81 @@
 import streamlit as st
-import boto3
+from langchain.chains import TransformChain
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+from langchain import globals
+from langchain_core.runnables import chain
+from langchain_core.output_parsers import JsonOutputParser, PydanticOutputParser
+from langchain_core.pydantic_v1 import BaseModel, Field
+import tempfile
 import os
-from dotenv import load_dotenv
-from PIL import Image
-from io import BytesIO
-from geopy.geocoders import Nominatim
-import json
+import base64
 from dotenv import load_dotenv
 load_dotenv(override=True)
-from langchain_openai import ChatOpenAI
-from langchain.output_parsers.openai_tools import JsonOutputToolsParser
-from typing import List
-from langchain.prompts import ChatPromptTemplate
 
+# Set verbose
+globals.set_debug(True)
 
-from pydantic import BaseModel, Field
-from typing import List
+def save_uploaded_image(uploaded_file):
+    """Salva l'immagine caricata in una directory temporanea e restituisce il percorso."""
+    if uploaded_file is not None:
+        # Creazione di un file temporaneo nella directory temporanea
+        with tempfile.NamedTemporaryFile(delete=False, suffix=os.path.splitext(uploaded_file.name)[1]) as temp_file:
+            temp_file.write(uploaded_file.read())
+            return temp_file.name
+    else:
+        return None
+
+def load_image(inputs: dict) -> dict:
+    """Load image from file and encode it as base64."""
+    image_path = inputs["image_path"]
+  
+    def encode_image(image_path):
+        with open(image_path, "rb") as image_file:
+            return base64.b64encode(image_file.read()).decode('utf-8')
+    image_base64 = encode_image(image_path)
+    return {"image": image_base64}
+
+load_image_chain = TransformChain(
+    input_variables=["image_path"],
+    output_variables=["image"],
+    transform=load_image
+)
+
+@chain
+def image_model(inputs: dict) -> str | list[str] | dict:
+ """Invoke model with image and prompt."""
+ model = ChatOpenAI(temperature=0.1, model="gpt-4o", max_tokens=1024)
+ msg = model.invoke(
+             [HumanMessage(
+             content=[
+             {"type": "text", "text": inputs["prompt"]},
+             {"type": "text", "text": parser.get_format_instructions()},
+             {"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{inputs['image']}"}},
+             ])]
+             )
+ return msg.content
+
+def get_image_informations(image_path: str) -> dict:
+   vision_prompt = """
+      Data l'immagine, fornisci le seguenti informazioni:
+      - Descrizione dettagliata della foto e del relativo problema ecologico. Fornire informazioni solo sul problema ecologico, non sull'ambiente circostante e non aggiungere altri commenti. Se la foto non riguarda un problema ecologico resituisci il messaggio "La foto non riguarda un problema ecologico". 
+      - Gravità (da 0 a 10) del problema ecologico, ad esempio gravità 9 o 10 in caso di grande discarica a cielo aperto . Se la foto non riguarda un problema ecologico resituisci 0. 
+      """
+   vision_chain = load_image_chain | image_model | parser
+   return vision_chain.invoke({'image_path': f'{image_path}', 
+                               'prompt': vision_prompt})
 
 class Report(BaseModel):
-    """Patient symptom described during a doctor's call."""
+    """Informazioni su un'immagine."""
+    descrizione: str = Field(description="Descrizione dettagliata del rapporto sul problema ecologico. Fornire informazioni solo sul problema ecologico, non sull'ambiente circostante e non aggiungere altri commenti")
+    gravità: int = Field(description="Gravità (da 0 a 10) del problema ecologico, ad esempio gravità 9 o 10 in caso di grande discarica a cielo aperto")
 
-    description: str = Field(description="Detailed description of the ecological problem report")
-    severity: str = Field(description="Severity (from 0 to 10) of the ecological problem, e.g. severity 9 or 10 in case of big open-air dump")
-   
+parser = PydanticOutputParser(pydantic_object=Report)
 
-model = ChatOpenAI(temperature=0).bind_tools([Report])
-prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", """
-            You are an assistant that helps a busy doctor who needs to quickly review and document symptoms described by patients during calls.
-            Your task is to carefully listen to the patient's description and extract all relevant symptoms.
-            For each symptom mentioned, provide the name of the symptom, a brief description, the duration for which the symptom has been present, its severity, and any additional notes or observations.
-            The final output should be a comprehensive and well-structured list of symptoms with all necessary details.
-            If the conversation is not inherent with the patient problem just return an empty list. 
-        """),
-        ("user", "{input}")
-    ]
-)
-parser = JsonOutputToolsParser()
-chain = prompt | model | parser
 
 def get_lat_long(address):
-    geolocator = Nominatim(user_agent="geoapiExercises")
-    location = geolocator.geocode(address)
-    if location:
-        return location.latitude, location.longitude
-    else:
-        return None, None
+    #TODO
+    pass
 
 # Function that processes the input and returns the JSON structure
 def process_report(input_text):
@@ -66,33 +95,15 @@ def main():
 
     # Campo per il caricamento della foto
     uploaded_file = st.file_uploader("Carica una foto del danno", type=["jpg", "jpeg", "png"])
+    image_path = save_uploaded_image(uploaded_file)
 
-    if st.button("Process Symptoms"):
-        if input_text.strip():
-            # Process the input through the function
-            symptoms_json = process_report(input_text)
-
-            # Check if the response is valid
-            if symptoms_json is not None:
-                if len(symptoms_json) == 0:
-                    st.error("Nessun sintomo individuato.")
-                else:
-                    try:
-                        # Display the extracted symptoms
-                        st.subheader("Extracted Symptoms")
-                        for symptom in symptoms_json:
-                            st.write(f"**Symptom Names:** {', '.join(symptom['args']['names'])}")
-                            st.write(f"**Description:** {symptom['args']['description']}")
-                            st.write(f"**Duration:** {symptom['args']['duration']}")
-                            st.write(f"**Severity:** {symptom['args']['severity']}")
-                            st.write(f"**Notes:** {symptom['args']['notes']}")
-                            st.write("---")
-                    except KeyError as e:
-                        st.error(f"Invalid JSON structure: missing key {e}")
-            else:
-                st.error("Failed to process the input.")
-        else:
-            st.warning("Please enter a description of symptoms before processing.")
+    if st.button("Send"):
+        result = get_image_informations(image_path)
+    
+    #Save on DB 
+        print(result.descrizione)
+        print(result.gravità)
+    # #TODO
 
 if __name__ == "__main__":
     main()
